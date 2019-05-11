e83c345cafd2729e494ddcad7f4aabf27ca012d8
Dennis Playdon <dennis9953@gmail.com>
2019-05-11 14:36:43 +1200

    create alphabet class with first letter

--- draw-paint.py
+++ draw-paint.py
@@ -1,6 +1,15 @@
 import pyautogui
 import time
 
+class Alphabet:
+    def drawH(self, startX, startY):
+        pyautogui.moveTo(startX, startY)
+        pyautogui.dragRel(0, 100)
+        pyautogui.moveRel(0, -50)
+        pyautogui.dragRel(50, 0)
+        pyautogui.moveRel(0, -50)
+        pyautogui.dragRel(0, 100)
+
 width, height = pyautogui.size()
 # open paint
 pyautogui.click(0, height, button='left')
@@ -8,3 +17,6 @@ pyautogui.click(0, height, button='left')
 pyautogui.typewrite('paint')
 time.sleep(1)
 pyautogui.press('enter')
+
+alphabet = Alphabet()
+alphabet.drawH(130, 301)
