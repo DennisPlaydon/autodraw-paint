f15a382beb414e9d1f701efb8e160a0f4dc729d5
Dennis Playdon <dennis9953@gmail.com>
2019-05-11 14:39:45 +1200

    write first word - hello

--- draw-paint.py
+++ draw-paint.py
@@ -2,6 +2,15 @@ import pyautogui
 import time
 
 class Alphabet:
+    def drawE(self, startX, startY):
+        pyautogui.moveTo(startX, startY)
+        pyautogui.dragRel(50, 0)
+        pyautogui.moveRel(-50, 0)
+        pyautogui.dragRel(0, 100)
+        pyautogui.dragRel(50, 0)
+        pyautogui.moveRel(-50, 0)
+        pyautogui.moveRel(0, -50)
+        pyautogui.dragRel(50, 0)
     def drawH(self, startX, startY):
         pyautogui.moveTo(startX, startY)
         pyautogui.dragRel(0, 100)
@@ -9,6 +18,16 @@ class Alphabet:
         pyautogui.dragRel(50, 0)
         pyautogui.moveRel(0, -50)
         pyautogui.dragRel(0, 100)
+    def drawL(self, startX, startY):
+        pyautogui.moveTo(startX, startY)
+        pyautogui.dragRel(0, 100)
+        pyautogui.dragRel(50, 0)
+    def drawO(self, startX, startY):
+        pyautogui.moveTo(startX, startY)
+        pyautogui.dragRel(50, 0)
+        pyautogui.dragRel(0, 100)
+        pyautogui.dragRel(-50, 0)
+        pyautogui.dragRel(0, -100)
 
 width, height = pyautogui.size()
 # open paint
@@ -20,3 +39,7 @@ pyautogui.press('enter')
 
 alphabet = Alphabet()
 alphabet.drawH(130, 301)
+alphabet.drawE(210, 301)
+alphabet.drawL(290, 301)
+alphabet.drawL(360, 301)
+alphabet.drawO(440, 301)
